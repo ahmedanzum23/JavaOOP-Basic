350daa21a48471d5ffbea9937631c0cae73f9db8
USER <USER@DESKTOP-JPREK6D>
2024-09-02 20:37:11 +0600

    2nd commit

--- Java_oop/src/java_oop/Oop1.java
+++ Java_oop/src/java_oop/Oop1.java
@@ -1,7 +1,32 @@
 package java_oop;
 
+
+class pen{
+	String name ;
+	int price ; 
+	
+	
+	
+	public void printpen() {
+		System.out.println(this.name);
+		System.out.println(this.price);
+		
+	}
+	
+	
+}
+
+
+
+
 public class Oop1 {
    public static void main (String[]args) {
-	   System.out.println("print hello world");
+	  pen p1 = new pen();
+	  p1. name = "Orange-pen" ;
+	  p1.price = 120;
+	  
+	  p1.printpen();
+	  
+	  
    }
 }
